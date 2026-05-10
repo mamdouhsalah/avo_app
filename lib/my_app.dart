34d4ef714842c62c11c93ad6e19ebbca4d9b3fe9
abdosaleh33445 <abdosaleh33445@gmail.com>
2026-05-10 16:55:20 +0300

    feat: complete payment feature with clean architecture

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,14 @@
-import 'package:avo_app/app/core/constants/app_strings.dart';
 import 'package:avo_app/app/core/theme/theme_app.dart';
-import 'package:avo_app/app/features/onboard/screens/onboard_screen.dart';
+import 'package:avo_app/app/features/profile/screens/profile_screen.dart';
+import 'package:avo_app/app/features/splash/screens/splash_screen.dart';
+import 'package:avo_app/app/features/home/data/home_data.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:avo_app/app/core/constants/app_strings.dart';
+
+import 'package:easy_localization/easy_localization.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,17 +21,28 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return DevicePreview(
-          enabled: !kReleaseMode,
-          builder: (context) => MaterialApp(
-            debugShowCheckedModeBanner: true,
-            title: AppStrings.appName,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: ThemeMode.system,
-            home: const OnboardScreen(),
-            locale: DevicePreview.locale(context),
-            builder: DevicePreview.appBuilder,
-            useInheritedMediaQuery: true,
+          enabled: true,
+          builder: (context) => MultiProvider(
+            providers: [
+              ChangeNotifierProvider(create: (_) => HomeViewModel()),
+            ],
+            child: MaterialApp(
+              // --- إعدادات اللغات بتاعتك ---
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+
+              debugShowCheckedModeBanner: false,
+              title: AppStrings.appName,
+              theme: AppTheme.lightTheme,
+              darkTheme: AppTheme.darkTheme,
+              themeMode: ThemeMode.system,
+
+              // --- شاشة البداية ---
+              home: const ProfileScreen(),
+
+              builder: DevicePreview.appBuilder,
+            ),
           ),
         );
       },
